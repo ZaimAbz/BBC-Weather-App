7453715ed41e3dd3cc2e3acb502cf18a60b2747c
ZaimAbz <zaimabodi@gmail.com>
2019-08-24 16:47:10 +0100

    fetching data from darksky API

--- app/src/componants/Cities.js
+++ app/src/componants/Cities.js
@@ -1,42 +1,56 @@
 import React, { Component } from "react";
 import City from "./City";
+import LondonImg from "../img/London.jpeg";
+import ParisImg from "../img/Paris.jpeg";
+import NewYorkImg from "../img/Newyork.jpeg";
+import SingaporeImg from "../img/Singapore.jpeg";
+import SydneyImg from "../img/Sydney.jpeg";
 
-const data = [
-  {
-    name: "London",
-    link:
-      "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    name: "Paris",
-    link:
-      "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80"
-  },
-  {
-    name: "New York",
-    link:
-      "https://images.unsplash.com/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-  },
-  {
-    name: "Singapore",
-    link:
-      "https://images.unsplash.com/photo-1525625293386-3f8f99389edd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1549&q=80"
-  },
-  {
-    name: "Sydney",
-    link:
-      "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-  }
-];
-
+  
 class Cities extends Component {
   render() {
     return (
       <div>
         <div className="Cities">
-          {data.map(city => {
-            return <City name={city.name} link={city.link} />;
-          })}
+          
+          <City
+            name="London"
+            image={LondonImg}
+            position= { {
+            latitude: 51.509865,
+          longitude: -0.093145
+        }}
+              />
+          <City
+            name= "Paris"
+            image={ParisImg}
+            position={{
+              latitude: 48.864716,
+              longitude: 2.349014}}
+          />
+          <City
+            name="New York"
+            image={NewYorkImg}
+            position={{
+              latitude: 40.7128,
+              longitude: -74.005974}}
+          />
+          <City
+            name="Singapore"
+            image={SingaporeImg}
+            position={{
+              latitude: 1.352083,
+              longitude: 103.819839
+            }}
+              />
+          <City
+            name="Sydney"
+            image={SydneyImg}
+            position={{
+              latitude: -33.86882,
+              longitude: 151.20929
+            }}
+              />
         </div>
       </div>
     );
--- app/src/componants/City.js
+++ app/src/componants/City.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import DarkSkyApi from "dark-sky-api";
+DarkSkyApi.apiKey = process.env.REACT_APP_BBC_WEATHER;
+
 
 class City extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loaded: false
+    };
+  }
+  componentDidMount() {
+    DarkSkyApi.loadCurrent(this.props.position).then(result => {
+      console.log(result)
+      this.setState({
+        data: result,
+        loaded: true
+      });
+    });
+  }
   render() {
     return (
       <div style={{ width: "18%" }}>
         <div className="Card">
           <div className="img-card">
-            <img calssName="city-img" src={this.props.link} />
+            <img calssName="city-img" src={this.props.image} />
           </div>
-          <hi className="card-title">{this.props.name}</hi>
+          <h1 className="card-title">{this.props.name}</h1>
+          <p>{this.state.loaded ? this.state.data.summary : null}</p>
+          <p>{this.state.loaded ? this.state.data.temperature : null}</p>
+          <p>{this.state.loaded ? this.state.data.icon : null}</p>
+          
         </div>
+
       </div>
     );
   }
